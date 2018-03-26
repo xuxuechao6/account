8f655f0a3b79fb15c4424b3203a659c600dbcd35
xuxuechao <xuxuechao@rt-thread.com>
2018-03-26 16:45:45 +0800

    修改 域名

--- controllers/oauth2/client/index.js
+++ controllers/oauth2/client/index.js
@@ -4,7 +4,7 @@ var clientSchema = require('../../../models/clientinfo').clientSchema;
 
 
 function clientRedirect(req,res) {
-        var authorization = 'http://localhost:3000/oauth2.0/authorize?response_type=code&client_id=';
+        var authorization = 'http://xxdemo.cn/oauth2.0/authorize?response_type=code&client_id=';
     var client = req.query.client;
     clientSchema(client,function (result) {
         var url = authorization + result.client_id +'&redirect_uri='+result.redirect_uri+'&state=233&scope=get_user_info,get_vip_info,get_vip_rich_info'
